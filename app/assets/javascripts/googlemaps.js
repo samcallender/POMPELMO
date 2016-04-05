9dc5c73c87df0a4bea9824c441176b29e890f685
Sam Callender <samcal84@gmail.com>
2016-04-05 17:27:53 -0400

    Weekly Backup

--- app/assets/javascripts/googlemaps.js
+++ app/assets/javascripts/googlemaps.js
@@ -685,5 +685,15 @@ var datePicker = function(){
 //     datePicker();
 // });
 
+// save this for default date formula
 
+// Date.prototype.yyyymmdd = function() {
+//    var yyyy = this.getFullYear().toString();
+//    var mm = (this.getMonth()+1).toString(); // getMonth() is zero-based
+//    var dd  = this.getDate().toString();
+//    return yyyy + (mm[1]?mm:"0"+mm[0]) + (dd[1]?dd:"0"+dd[0]); // padding
+//   };
+
+// d = new Date();
+// d.yyyymmdd();
 
--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,22 @@
 class StaticPagesController < ApplicationController
   def home
+  	@missed_connections = MissedConnection.all.sample(3)
+  	@mc2 = @missed_connections[1]
   end
 
   def about
+  end
+
+  def map
+  	now = Date.today
+	seven_days_ago = (now - 7)
+	a = Date.parse(seven_days_ago.to_s).to_s
+	yyyy = a[0..3]
+    mm = a[5..6]
+    dd = a[8..9]
+    @start_date = mm+'/'+dd+'/'+yyyy
+
+  	b = Date.parse(Time.now.to_s)
+    @end_date = (b >> 1).strftime("%d/%m/%Y %H:%M")[1..10]
   end
 end
